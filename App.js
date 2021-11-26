5f1f5074968d036255a065b9a8f54a68d67e2870
TWINKE IGNASIUS <TP054187@mail.apu.edu.my>
2021-11-26 08:40:42 +0800

    twinke updated testing

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Testing only</Text> 
+      {/* Twinke commented */}
     </View>
   );
 }
